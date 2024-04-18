5fa28e6299836c365053b9965509b2213f508315
Etho <58352929+EthoIRL@users.noreply.github.com>
2024-04-17 20:52:36 -0700

    Improved UDP server & updated to new HID-API bindings

--- Game/ServerHandler.cs
+++ Game/ServerHandler.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime;
 using Helious.Utils;
 using HID_API;
 
@@ -10,88 +9,88 @@ namespace Helious.Game;
 public class ServerHandler
 {
     private readonly bool _hasMouse;
-    private readonly EndPoint _localEndpoint = new IPEndPoint(IPAddress.Any, 7483);
+    private static readonly IPAddress Broadcast = IPAddress.Parse("192.168.0.190");
+    private readonly EndPoint _localEndpoint = new IPEndPoint(Broadcast, 7483);
+    private readonly List<long> _timingR = new();
+    private readonly List<long> _timingS = new();
 
     public ServerHandler(HidHandler hidHandler)
     {
-        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        socket.Bind(_localEndpoint);
-
-        // Stopwatch stopwatch = new Stopwatch();
-        Span<byte> bytes = GC.AllocateArray<byte>(9, true);
-
         while (true)
         {
-            if (GCSettings.LatencyMode != GCLatencyMode.NoGCRegion)
-            {
-                GC.TryStartNoGCRegion((1024 * 1024) * 64, (1024 * 1024) * 16, false);
-            }
-
             if (!_hasMouse)
             {
                 if (hidHandler.HidMouseHandlers.Count > 0)
                 {
                     _hasMouse = true;
+                    break;
                 }
-                else
-                {
-                    Thread.Sleep(1);
-                    continue;
-                }
+
+                Thread.Sleep(1);
             }
+        }
+    
+        using Socket listener = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        listener.Blocking = false;
+        listener.Bind(_localEndpoint);
 
-            socket.ReceiveFrom(bytes, SocketFlags.None, ref _localEndpoint);
-
-            if (bytes.Length > 0)
+        Span<byte> bytes = GC.AllocateArray<byte>(8, true);
+        Stopwatch sending = new Stopwatch();
+        Stopwatch reading = new Stopwatch();
+        
+        if (hidHandler.HidPath == null)
+        {
+            ConsoleUtils.WriteLine("Couldn't create hid stream; path not found.");
+            return;
+        }
+        var hidStream = hidHandler.CreateHidStream("/dev/hidg1");
+        
+        while (true)
+        {
+            if (listener.Available != 0)
             {
-                int centerX = (short) (bytes[0] | bytes[1] << 8);
-                int centerY = (short) (bytes[2] | bytes[3] << 8);
-                bool ignoreAim = bytes.Length > 4 && bytes[4] > 0;
-                bool ignoreWait = bytes.Length > 5 && bytes[5] > 0;
+                var received = listener.Receive(bytes);
 
-                bool? left = bytes.Length > 6 ? bytes[6] > 0 ? true : null : null;
-                bool? right = bytes.Length > 7 ? bytes[7] > 0 ? true : null : null;
-                bool? middle = bytes.Length > 8 ? bytes[8] > 0 ? true : null : null;
-
-                if (_hasMouse)
+                if (received != 0)
                 {
-                    if (RecoilHandler.Aiming | ignoreAim)
+                    short centerX = (short) (bytes[0] | bytes[1] << 8);
+                    short centerY = (short) (bytes[2] | bytes[3] << 8);
+                    var ignoreAim = bytes.Length > 4 && bytes[4] > 0;
+
+                    bool? left = bytes.Length > 5 ? bytes[5] > 0 ? true : null : null;
+                    bool? right = bytes.Length > 6 ? bytes[6] > 0 ? true : null : null;
+                    bool? middle = bytes.Length > 7 ? bytes[7] > 0 ? true : null : null;
+
+                    reading.Restart();
+                    var mouseState = hidHandler.HidMouseHandlers[0].GetMouseState();
+                    reading.Stop();
+                    
+                    sending.Restart();
+                    if (mouseState.RightButton | ignoreAim)
                     {
-                        if (RecoilHandler.WaitAiming.ElapsedMilliseconds >= 550 | ignoreWait)
+                        left ??= mouseState.LeftButton;
+                        right ??= mouseState.RightButton;
+                        middle ??= mouseState.MiddleButton;
+                    
+                        hidHandler.WriteMouseReport(mouseState with
                         {
-                            // stopwatch.Restart();
-                            hidHandler.HidMouseHandlers[0].MouseLock.EnterReadLock();
-                            try
-                            {
-                                left ??= hidHandler.HidMouseHandlers[0].Mouse.LeftButton;
-                                right ??= hidHandler.HidMouseHandlers[0].Mouse.RightButton;
-                                middle ??= hidHandler.HidMouseHandlers[0].Mouse.MiddleButton;
-
-                                hidHandler.WriteGenericEvent(hidHandler.HidMouseHandlers[0].Mouse with
-                                {
-                                    LeftButton = (bool) left,
-                                    RightButton = (bool) right,
-                                    MiddleButton = (bool) middle,
-                                    X = centerX,
-                                    Y = centerY,
-                                    Wheel = 0
-                                });
-
-                                // stopwatch.Stop();
-
-                                // ConsoleUtils.WriteLine($"Elapsed since packet received: {stopwatch.ElapsedTicks}");
-                            }
-                            finally
-                            {
-                                hidHandler.HidMouseHandlers[0].MouseLock.ExitReadLock();
-                            }
-                        }
+                            LeftButton = (bool) left,
+                            RightButton = (bool) right,
+                            MiddleButton = (bool) middle,
+                            X = centerX,
+                            Y = centerY,
+                            Wheel = 0
+                        }, hidStream);
                     }
-                }
+                    sending.Stop();
+                    
+                    _timingR.Add(reading.ElapsedTicks);
+                    _timingS.Add(sending.ElapsedTicks);
 
-                if (GCSettings.LatencyMode == GCLatencyMode.NoGCRegion)
-                {
-                    GC.EndNoGCRegion();
+                    if (_timingR.Count != 0 && _timingS.Count != 0 && _timingR.Count % 100 == 0)
+                    {
+                        ConsoleUtils.WriteLine($"Reading: {_timingR.Average() / 10000.0}, Sending: {_timingS.Average() / 10000.0}");
+                    }
                 }
             }
         }
